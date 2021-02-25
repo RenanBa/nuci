7c0b260f0575a3d5c412b0e057a971b23e168faf
RenanBa <renanbarbosadesouza@gmail.com>
2021-02-25 15:11:01 -0800

    Add shop and all_item methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < ApplicationController
+
+  def shop
+    # optimize to returns the 9 next products instead of all products
+    @products = Products.all
+    # all categories also
+  end
+
+  def all_items
+    # all_items should return 9 most popular products of each category
+    # it also should return all categories name
+  end
+
 end
