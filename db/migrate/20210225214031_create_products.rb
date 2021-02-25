f3d88d5a2bbcc087c872cd9290e612891c6cf8cf
RenanBa <renanbarbosadesouza@gmail.com>
2021-02-25 13:51:39 -0800

    Add products details migration

--- db/migrate/20210225214031_create_products.rb
+++ db/migrate/20210225214031_create_products.rb
@@ -1,8 +1,17 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
+      t.string :name
+      t.string :size
+      t.decimal :price
+      t.integer :quantity
+      t.string :color
+      t.text :description
+      t.integer :rating
+      t.string :image_url
+      t.integer :category_id
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
