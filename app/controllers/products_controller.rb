16759404d0cf847ef849afcfa76eaade4d5a7c02
RenanBa <renanbarbosadesouza@gmail.com>
2021-02-25 16:05:41 -0800

    Add link to shop and Create products index view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,7 @@
 class ProductsController < ApplicationController
-
-  def shop
+  def index
     # optimize to returns the 9 next products instead of all products
-    @products = Products.all
+    @products = Product.all
     # all categories also
   end
-
-  def all_items
-    # all_items should return 9 most popular products of each category
-    # it also should return all categories name
-  end
-
 end
