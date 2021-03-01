33f5783e13897d430ddeb16d2b7e7f4fd223eba1
RenanBa <renanbarbosadesouza@gmail.com>
2021-02-28 23:21:30 -0800

    Add show route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,12 @@
 class ProductsController < ApplicationController
+  # GET /products
   def index
     # optimize to returns the 9 next products instead of all products
     @products = Product.all
     # all categories also
+  end
+
+  # GET /product/1
+  def show
   end
 end
