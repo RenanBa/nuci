4f166c6dbb1bf09aecae4967e88280cf0d4f0891
RenanBa <renanbarbosadesouza@gmail.com>
2021-02-28 21:29:56 -0800

    Add seed and adjust products display

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times {
+  product = Product.create(
+    name: "Neckless",
+    size: "M",
+    price: 5.0,
+    quantity: 3,
+    color: "Red",
+    description: "Product created on seed file for testing purpose",
+    rating: 8,
+    image_url: "images/shop/products/product-1.jpg",
+    category_id: "Jewe"
+    )
+}
