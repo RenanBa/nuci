9bd1ac7ffb3cfafd3ebdb058ebbb6c74fee2ebd1
RenanBa <renanbarbosadesouza@gmail.com>
2021-03-02 15:39:28 -0800

    Add protect from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit({ roles: [] }, :email, :password, :password_confirmatio, :name)
     end
-  end
 
-  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
       user_params.permit({ roles: [] }, :email, :password, :password_confirmatio, :current_password, :name)
     end
   end
+
+  protect_from_forgery with: :exception
 
 end
