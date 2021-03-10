3076c654e33680b28a7611c6fcd1ee8e117c4aab
RenanBa <renanbarbosadesouza@gmail.com>
2021-03-10 07:55:21 -0800

    Mingrate LineItem and Order models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_001509) do
+ActiveRecord::Schema.define(version: 2021_03_08_182714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "quantity"
+    t.decimal "line_item_total"
+    t.integer "order_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "costumer_key"
+  end
+
+  create_table "oders", force: :cascade do |t|
+    t.decimal "subtotal"
+    t.decimal "sales_tax"
+    t.decimal "grand_total"
+    t.integer "user_id"
+    t.text "order_items"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
