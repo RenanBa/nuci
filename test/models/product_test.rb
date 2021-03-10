659f8a4d7d9103af1cc4b95f0992ac5e172f6687
RenanBa <renanbarbosadesouza@gmail.com>
2021-03-10 15:28:02 -0800

    Validate if product attributes before save

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Shouldn't save without the product's main attributes" do
+    product = Product.new
+    assert_not product.save, "Saved a new product without a name, size, price, quantity, description, image_url"
+  end
 end
