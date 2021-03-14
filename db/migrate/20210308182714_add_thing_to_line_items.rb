d50a97a337d0fe77954f03afd22380a8842aa91a
RenanBa <renanbarbosadesouza@gmail.com>
2021-03-13 19:38:33 -0800

    update row name

--- db/migrate/20210308182714_add_thing_to_line_items.rb
+++ db/migrate/20210308182714_add_thing_to_line_items.rb
@@ -1,5 +1,6 @@
 class AddThingToLineItems < ActiveRecord::Migration[6.1]
   def change
-    add_column :line_items, :costumer_key, :string
+    add_column :line_items, :customer_key, :string
   end
 end
+`
