09919af47c289d144f0895957fa8c074d5f2d8b9
RenanBa <renanbarbosadesouza@gmail.com>
2021-03-22 16:17:06 -0700

    Update add to cart method to only add to cart when user is logged in. It will need to be update to not require the user to login before

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,6 +2,10 @@ class CartController < ApplicationController
   before_action :authenticate_user!, except: [:add_to_cart, :view_order]
 
   def add_to_cart
+    # If user add to cart from products page or store_front page the quantity
+    # will be nil. In this case we set the quantity to 1
+    params[:qty] = 1 if params[:qty] == nil
+
     product = Product.find(params[:product_id])
 
     if product.quantity < params[:qty].to_i
@@ -11,16 +15,31 @@ class CartController < ApplicationController
       line_item.product_id = params[:product_id].to_i
       line_item.quantity = params[:qty]
     end
+
     if user_signed_in?
       line_item.customer_key = current_user.id
+
+      # At this point we also need a Order to link the LineItem with. So we will
+      # create one order before saving the LineItem
+      order = Order.new
+      order.user_id = line_item.customer_key
+      order.save!
+      # Now we can save LineItem with the newly created order
+      line_item.order_id = order.id
+      line_item.save
+
+      line_item.line_item_total = line_item.quantity * line_item.product.price
+      line_item.save
+
+      redirect_to view_order_path
+
     else
-      line_item.customer_key = remote_ip
+      redirect_to new_user_session_path, notice: "You must be logged in to add products to your cart."
+      # line_item.customer_key = "127.0.0.1" #need remote_ip function to return IP
     end
-    line_item.save
+  end
 
-    line_item.line_item_total = line_item.quantity * line_item.product.price
-    line_item.save
-
-    redirect_to view_order_path
+  def view_order
+    @line_items = LineItem.where(customer_key: current_user.id)
   end
 end
